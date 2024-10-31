d5cd5b3e0a891a9aa0e63c4056814d5941f8ceba
Ahad Ali <ahadali0500@gmail.com>
2024-10-30 22:41:40 -0700

    d

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7001;
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
